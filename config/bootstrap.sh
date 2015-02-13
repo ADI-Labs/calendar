a7082987da6efd666d6568d9c39352f224cb9f97
David H <davidhao3300@yahoo.com>
2015-02-13 15:07:39 -0500

    First iteration of structure

---
+++ config/bootstrap.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# installs the package passed in if it's not installed
+install () {
+    package=$1
+    dpkg-query -l $package &> /dev/null
+    if [ $? -ne 0 ]; then
+        apt-get -y install $package
+    fi
+}
+
+apt-get update
+
+# install git
+install git-core
+install git
+
+# install python
+apt-get install -y python \
+    python-pip \
+    python-dev \
+    python-software-properties
+
+apt-get update
+sudo pip install -r /vagrant/config/requirements.txt
+pip install flake8  # for local testing
+
+
+# install vim
+install vim
+
+exit 0
