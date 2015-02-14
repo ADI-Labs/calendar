bd3aa6154d02e3724e7fc54545500e82a9e52e21
Alan Du <alanhdu@gmail.com>
2015-02-13 19:45:24 -0500

    Fixup vagrant setup

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -21,9 +21,7 @@ apt-get install -y python \
     python-dev \
     python-software-properties
 
-apt-get update
 sudo pip install -r /vagrant/config/requirements.txt
-pip install flake8  # for local testing
 
 # install vim
 apt-get install -y vim
