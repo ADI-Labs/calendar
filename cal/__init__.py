218368f694ce62552bbb1d67856ba58cb340c16d
David H <davidhao3300@yahoo.com>
2015-03-06 14:46:36 -0500

    Remove unnecessary definitions

--- cal/__init__.py
+++ cal/__init__.py
@@ -5,36 +5,26 @@ from schema import db, Event, User
 from cal.fb import update_fb_events
 from celery import Celery
 
-def make_celery(app):
-    # Create the celery app.
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
 
-def create_app():
-    # Create the Flask app.
-    app = Flask(__name__)
-    app.config.from_object('config')
-    db.init_app(app)
-    whooshalchemy.whoosh_index(app, Event)
-    return app
+app = Flask(__name__)
+app.config.from_object('config')
+db.init_app(app)
+whooshalchemy.whoosh_index(app, Event)
 
-app = create_app()
-celery = make_celery(app)
+celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+TaskBase = celery.Task
+class ContextTask(TaskBase):
+    abstract = True
+    def __call__(self, *args, **kwargs):
+        with app.app_context():
+            return TaskBase.__call__(self, *args, **kwargs)
+celery.Task = ContextTask
 
 # Background task
 @celery.task(name="cal.fb_task")
 def fb_task_test():
-    print "Starting FB update"
     update_fb_events()
-    print "Finished FB update"
     # Update every 30 minutes
     fb_task_test.apply_async(countdown=1800)
 
