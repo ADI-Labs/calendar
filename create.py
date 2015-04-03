f9097e0a82f5d655cda6f8a15e67018433a1d272
David H <davidhao3300@yahoo.com>
2015-04-03 14:45:30 -0400

    Pep8

--- create.py
+++ create.py
@@ -6,12 +6,11 @@ import yaml
 with app.app_context():
     db.create_all()
     with open('cal/groups.yml') as fin:
-      users = yaml.load(fin)
+        users = yaml.load(fin)
     for username, ids in users.iteritems():
-      if User.query.filter_by(name=username).first() is None:
-        new_user = User(name=username)
-        if 'fb' in ids:
-          new_user.fb_id = ids['fb']
-        db.session.add(new_user)
-        db.session.commit()
+        if User.query.filter_by(name=username).first() is None:
+            new_user = User(name=username)
+            new_user.fb_id = ids.get('fb', None)
+            db.session.add(new_user)
+            db.session.commit()
     update_fb_events()
