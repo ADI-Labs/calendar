3ecd40c8c6b2f6ddfe29103cfbdbeb2195deb03b
Jonathan <sunyang713@hotmail.com>
2015-10-02 16:22:31 -0400

    Converts tab-indentation to spaces. Sets tab-width to 2.

--- cal/static/jsx/app.jsx
+++ cal/static/jsx/app.jsx
@@ -1,83 +1,81 @@
 var toDateString = function(date) {
-    return (date.getFullYear().toString() + "/" +
+  return (date.getFullYear().toString() + "/" +
             (date.getMonth() + 1).toString() + "/" + // +1 b/c js months start at 0
             date.getDate().toString());
 };
 
 var RApp = React.createClass({
-    getInitialState: function() {
-        return {eventList: [], userList: [], 
-                date: new Date(), allEventList: []};
+  getInitialState: function() {
+    return {eventList: [], userList: [], 
+      date: new Date(), allEventList: []};
     },
 
     componentDidMount: function() {
-        $.getJSON("/events/" + toDateString(this.state.date), function(data) {
-            this.setState({eventList: data.data, allEventList: data.data});
-        }.bind(this));
+      $.getJSON("/events/" + toDateString(this.state.date), function(data) {
+        this.setState({eventList: data.data, allEventList: data.data});
+      }.bind(this));
 
-        $.getJSON("/users/" + toDateString(this.state.date), function(data) {
-            this.setState({userList: data.data});
-        }.bind(this));
+      $.getJSON("/users/" + toDateString(this.state.date), function(data) {
+        this.setState({userList: data.data});
+      }.bind(this));
     },
 
     addUser: function(user_id) {
-        var newEvents = this.state.allEventList.filter(function (evt) {
-            return evt.user_id == user_id;
-        });
-        this.setState({
-            eventList: this.state.eventList.concat(newEvents)
-        });
+      var newEvents = this.state.allEventList.filter(function (evt) {
+        return evt.user_id == user_id;
+      });
+      this.setState({
+        eventList: this.state.eventList.concat(newEvents)
+      });
     },
 
     removeUser: function(user_id) {
-        this.setState({
-            eventList:  this.state.eventList.filter(function (evt) {
-                            return evt.user_id != user_id;
-                        })
-        });
+      this.setState({
+        eventList:  this.state.eventList.filter(function (evt) {
+          return evt.user_id != user_id;
+        })
+      });
     },
 
     setDate: function(date) {
-        var state = this.state;
-        state.date = date;
-        this.setState(state);
-        $.getJSON("/events/" + toDateString(this.state.date), function(data) {
-            this.setState({eventList: data.data, allEventList: data.data});
-        }.bind(this));
+      var state = this.state;
+      state.date = date;
+      this.setState(state);
+      $.getJSON("/events/" + toDateString(this.state.date), function(data) {
+        this.setState({eventList: data.data, allEventList: data.data});
+      }.bind(this));
 
-        $.getJSON("/users/" + toDateString(this.state.date), function(data) {
-            this.setState({userList: data.data});
-        }.bind(this));
+      $.getJSON("/users/" + toDateString(this.state.date), function(data) {
+        this.setState({userList: data.data});
+      }.bind(this));
     },
 
     incrementDate: function(days) {
-        var date = this.state.date;
-        date.setDate(date.getDate() + days);
-        this.setDate(date);
+      var date = this.state.date;
+      date.setDate(date.getDate() + days);
+      this.setDate(date);
     },
 
     render: function() {
-        return (
-            <div className="app">
-                <RSearch setGlobalState={this.setState}
-                    eventList={this.state.eventList}
-                    incrementDate={this.incrementDate}
-                    date = {this.state.date}
-                />
-                <RCalendar eventList={ this.state.eventList } 
-                    incrementDate = {this.incrementDate}
-                    date = {this.state.date}/>
-                <RQuery eventList={this.state.eventList} 
-                    userList={this.state.userList} 
-                    removeUser={this.removeUser} 
-                    addUser={this.addUser}
-                    setGlobalState={this.setState} 
-                    date = {this.state.date}
-                    setDate = {this.setDate}
-                 />
-            </div>
+      return (
+        <div className="app">
+          <RSearch setGlobalState={this.setState}
+            eventList={this.state.eventList}
+            incrementDate={this.incrementDate}
+            date = {this.state.date}/>
+          <RCalendar eventList={ this.state.eventList } 
+            incrementDate = {this.incrementDate}
+            date = {this.state.date}/>
+          <RQuery eventList={this.state.eventList} 
+            userList={this.state.userList} 
+            removeUser={this.removeUser} 
+            addUser={this.addUser}
+            setGlobalState={this.setState} 
+            date = {this.state.date}
+            setDate = {this.setDate}/>
+        </div>
         );
     }
-});
+  });
 
 React.render(<RApp/>, document.getElementById("content"));
