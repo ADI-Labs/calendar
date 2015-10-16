2ddfacb376cd9a4b4f6b54c27f93fe533d8f29b8
Jonathan <sunyang713@hotmail.com>
2015-10-16 16:32:37 -0400

    Clean vagrant bootstrap - fix broken dependencies & install order.

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -2,26 +2,21 @@
 
 # installs the package passed in if it's not installed
 install () {
-    package=$1
-    dpkg-query -l $package &> /dev/null
-    if [ $? -ne 0 ]; then
-        apt-get -y install $package
-    fi
+  package=$1
+  dpkg-query -l $package &> /dev/null
+  if [ $? -ne 0 ]; then
+    apt-get -y install $package
+  fi
 }
 
 apt-get update
+apt-get upgrade
 
-install sqlite3
-
-# install python
+install git
 install python3
 install python3-dev
-install python3-software-properties
-install git
-
-wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-sudo python3 get-pip.py
-rm get-pip.py
+install python3-pip
+install sqlite3
 
 sudo pip3 install -r /vagrant/config/requirements.txt
 
