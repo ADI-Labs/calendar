458351b31e3ce9f9acbd977d7ba6e0211e8ea8ee
tarapichumani <tarapichumani96@gmail.com>
2016-03-25 15:12:28 -0400

    Document query string

--- cal/events-at-cu.py
+++ cal/events-at-cu.py
@@ -1,24 +1,29 @@
 import requests
 from icalendar import Calendar
-# from cal.schema import db, User, Event
+from cal.schema import db, User, Event
 
 def update_from_eventsatcu():
 
-    url = "https://events.columbia.edu/feeder/main/eventsFeed.gdo?sort=dtstart.utc:asc&format=text/calendar&count=200"
+    ''' sort = dtstart.utc - sorts by start date
+    format=text/calendar - to return in ics format
+    count = 200 - to return max number of events '''
+
+    url = "https://events.columbia.edu/feeder/main/eventsFeed.gdo" + \
+           "?sort=dtstart.utc:asc&format=text/calendar&count=200"
     r = requests.get(url)
     cal = Calendar.from_ical(r.text)
     user = User.query.filter(User.name == "events.columbia.edu").first() 
 
     for event in cal.walk('VEVENT'):
-        print(event.get('uid'))
-    
+
         event_id = event.get('uid')
         event_name = event.get('description')
         event_url = event.get('url')
 
         cevent = Event.query.filter(Event.sundial_id == event_id).first()
         if cevent is None:
-            cevent = Event(name = event_name, url = url, sundial_id = event_id, user_id = user.id)
+            cevent = Event(name = event_name, url = url, 
+                           sundial_id = event_id, user_id = user.id)
 
         cevent.start = event.get('dtstart').dt.replace(tzinfo=None)
         cevent.end = event.get('dtend').dt.replace(tzinfo=None)
@@ -29,5 +34,3 @@ def update_from_eventsatcu():
         
     db.session.commit()
 
-
-update_from_eventsatcu()
