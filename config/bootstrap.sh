862282c65f296cd90c1a20ec651b2b2e28948a97
Andyman <ajo0131@gmail.com>
2016-03-25 16:09:47 -0400

    Add SQL setup commands to bootstrap

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -15,7 +15,6 @@ apt-get upgrade
 install git
 install sqlite3
 
-
 install python3
 install python3-dev
 install python3-pip
@@ -23,5 +22,10 @@ install python3-pip
 #install postgresql-server-dev-all
 install postgresql-9.3
 install libpq-dev
+
+sudo -u postgres psql -c "CREATE USER adi_calendar WITH PASSWORD 'calpwd';"
+sudo -u postgres psql -c "CREATE DATABASE calendar;"
+sudo -u postgres psql -c "GRANT CONNECT ON DATABASE calendar TO adi_calendar;"
+sudo -u postgres psql -c "GRANT ALL ON DATABASE calendar TO adi_calendar;"
 
 sudo pip3 install -r /vagrant/config/requirements.txt
